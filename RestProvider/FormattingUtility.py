9e5089c618e4d3c997755c319a6215626d6f0795
Simon Geyer <simon@bananabook.fritz.box>
2016-07-24 14:49:57 +0200

    Added getDict-Method

--- RestProvider/FormattingUtility.py
+++ RestProvider/FormattingUtility.py
@@ -2,9 +2,12 @@ import json
 
 #### Static utility functions for processing json information
 #### -F
+def getDict(jsonString):
+    return json.loads(jsonString)
 
-def getFormattedJSON(jsonString):
-    return format(json.dumps(jsonString))
+def getFormattedJSON(data):
+    # return format(json.dumps(jsonString))
+    return json.dumps(data)
 
 def format(string):
     return string.decode('string_escape')
