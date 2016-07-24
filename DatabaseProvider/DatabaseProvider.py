85494f3ae4205a13fc841a23922714c7b2396486
Simon Geyer <simon@bananabook.fritz.box>
2016-07-24 14:03:47 +0200

    Added cardId row

--- DatabaseProvider/DatabaseProvider.py
+++ DatabaseProvider/DatabaseProvider.py
@@ -1,27 +1,29 @@
-import sqlite3
+# This Python file uses the following encoding: utf-8
 
+import sqlite3
 
 class DatabaseProvider:
     connection = sqlite3.connect("magic.db")
 
     cursor = connection.cursor()
 
-    sql_command = """
-    CREATE TABLE cards (
-    id INTEGER PRIMARY KEY;"""
+    sql_command = '''
+    CREATE TABLE IF NOT EXISTS cards
+    (id INTEGER PRIMARY KEY,
+    cardId INTEGER)'''
     # je nachdem was noch eingefügt werden sollte
 
     cursor.execute(sql_command)
 
 
-def addToCollection(card):
+def addToCollection(cardId):
     connection = sqlite3.connect("magic.db")
     cursor = connection.cursor()
 
-    format_str = """INSERT INTO cards (id)
-        VALUES ("{id}");"""
+    format_str = '''INSERT INTO cards (cardId)
+        VALUES ("{cardId}")'''
 
-    sql_command = format_str.format(id=card)
+    sql_command = format_str.format(cardId=cardId)
 
     cursor.execute(sql_command)
 
